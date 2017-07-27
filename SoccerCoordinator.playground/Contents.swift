bb712c5befc71209789a939f592a3ec1851537c2
BetaBry <betabry42@icloud.com>
2017-07-27 18:40:53 -0400

    Declaring player collections

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,7 +1,26 @@
-//: Playground - noun: a place where people can play
 
-import UIKit
+// Declaring player data
 
-var str = "Hello, playground"
+let player1: [String: Any] = ["name": "Joe Smith", "height": 42, "experienced": true, "guardian": "Jim and Jan Smith"]
+let player2: [String: Any] = ["name": "Jill Tanner", "height": 36, "experienced": true, "guardian": "Clara Tanner"]
+let player3: [String: Any] = ["name": "Bill Bon", "height": 43, "experienced": true, "guardian": "Sara and Jenny Bon"]
+let player4: [String: Any] = ["name": "Eva Gordon", "height": 45, "experienced": false, "guardian": "Wendy and Mike Gordon"]
+let player5: [String: Any] = ["name": "Matt Gill", "height": 40, "experienced": false, "guardian": "Charles and Sylvia Gill"]
+let player6: [String: Any] = ["name": "Kimmy Stein", "height": 41, "experienced": false, "guardian": "Bill and Hillary Stein"]
+let player7: [String: Any] = ["name": "Sammy Adams", "height": 45, "experienced": false, "guardian": "Jeff Adams"]
+let player8: [String: Any] = ["name": "Karl Saygan", "height": 42, "experienced": true, "guardian": "Heather Bledsoe"]
+let player9: [String: Any] = ["name": "Suzane Greenberg", "height": 44, "experienced": true, "guardian": "Henrietta Dumas"]
+let player10: [String: Any] = ["name": "Sal Dali", "height": 41, "experienced": false, "guardian": "Gala Dali"]
+let player11: [String: Any] = ["name": "Joe Kavalier", "height": 39, "experienced": false, "guardian": "Sam and Elaine Kavalier"]
+let player12: [String: Any] = ["name": "Ben Finkelstein", "height": 44, "experienced": false, "guardian": "Aaron and Jill Finkelstein"]
+let player13: [String: Any] = ["name": "Diego Soto", "height": 41, "experienced": true, "guardian": "Robin and Sarika Soto"]
+let player14: [String: Any] = ["name": "Chloe Alaska", "height": 41, "experienced": false, "guardian": "David and Jamie Alaksa"]
+let player15: [String: Any] = ["name": "Arnold Willis", "height": 43, "experienced": false, "guardian": "Claire Willis"]
+let player16: [String: Any] = ["name": "Phillip Helm", "height": 44, "experienced": true, "guardian": "Thomas Helm and Eva Jones"]
+let player17: [String: Any] = ["name": "Les Clay", "height": 42, "experienced": true, "guardian": "Wynonna Brown"]
+let player18: [String: Any] = ["name": "Herschel Krustofski", "height": 45, "experienced": true, "guardian": "Hyman and Rachel Krustofski"]
 
-// First Comment
+let players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+
+
